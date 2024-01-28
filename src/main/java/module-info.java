61122c08640ff30b89b55a3f2d64137e815d14da
Ketan Pal <88307951+Ketan-Pal@users.noreply.github.com>
2024-01-28 15:29:51 +0530

    base struc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
 module com.example.mazebank {
     requires javafx.controls;
     requires javafx.fxml;
+    requires fontawesomefx;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+
 
 
     opens com.example.mazebank to javafx.fxml;
     exports com.example.mazebank;
+    exports  com.example.mazebank.Controllers;
+    exports com.example.mazebank.Controllers.Admin;
+    exports com.example.mazebank.Controllers.Client;
+    exports com.example.mazebank.Models;
+    exports com.example.mazebank.Views;
 }
